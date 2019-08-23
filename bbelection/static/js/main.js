d2208a3df12e9300e7d3d4818e17b120c7a9304f
Stanley Sham <stanley.sham94@gmail.com>
2019-08-23 14:23:17 -0400

    completed assignment

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    const candidates = document.querySelector('.candidates');
+    const candidatesRequest = axios.get('https://bb-election-api.herokuapp.com/');
+    candidatesRequest.then(function(response){
+        for (let i = 0; i < response.data['candidates'].length; i++){
+            console.log(response.data['candidates'][i]);
+            let candidate = response.data['candidates'][i];
+            let candidateEntry = document.createElement('li');
+            candidateEntry.innerHTML = `${candidate['name']} - ${candidate['votes']} Votes`;
+            candidates.appendChild(candidateEntry)
+        };
+    })
 });
